a14f7db9a31c758fe0782616d4e07ec289cf3d82
Miisiekkk <radziejowskimichal@gmail.com>
2019-04-28 21:46:06 +0200

    React basic configuration

---
+++ src/main/java/Miisiekkk/RestApi/HomeController.java
@@ -0,0 +1,13 @@
+package Miisiekkk.RestApi;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
