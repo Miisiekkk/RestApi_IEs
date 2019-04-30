9c94d301e82bef9e3777cebadda9d04a73cfb584
Miisiekkk <radziejowskimichal@gmail.com>
2019-04-30 23:10:57 +0200

    Added Hypermedia controls

--- src/main/java/Miisiekkk/RestApi/DatabaseLoader.java
+++ src/main/java/Miisiekkk/RestApi/DatabaseLoader.java
@@ -17,6 +17,10 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+
+        this.repository.save(new Employee("Frodo", "Wagins", "tuman"));
+        this.repository.save(new Employee("DilDo", "Swaggins", "cham"));
+        this.repository.save(new Employee("Grzegorz", "Duszyk", "cymbał"));
+
     }
 }
