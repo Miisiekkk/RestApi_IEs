5b1cd751bccacf70e7de99bd32567652fa1154fc
Miisiekkk <radziejowskimichal@gmail.com>
2019-07-02 15:34:22 +0200

    Security configuration file written, with manager repository

---
+++ src/main/java/Miisiekkk/RestApi/ManagerRepository.java
@@ -0,0 +1,17 @@
+
+package Miisiekkk.RestApi;
+
+import org.springframework.data.repository.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+
+@RepositoryRestResource(exported = false)
+public interface ManagerRepository extends Repository<Manager, Long> {
+
+	Manager save(Manager manager);
+
+	Manager findByName(String name);
+
+}
+
